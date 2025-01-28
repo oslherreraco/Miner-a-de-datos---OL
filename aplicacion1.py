014c40a0912ca7dcd7d7ead06390bec9ca75902d
oslherreraco <oslherreraco@unal.edu.co>
2025-01-28 18:41:45 -0500

    Update aplicacion1.py

--- aplicacion1.py
+++ aplicacion1.py
@@ -1,7 +1,7 @@
+import numpy as np
 import streamlit as st
 from PIL import Image
 from tensorflow.keras.preprocessing.image import img_to_array
-import numpy as np
 
 def preprocess_image(image):
   image = image.convert('L') # convertir a escala de grises
@@ -14,7 +14,24 @@ def main():
   st.title("Clasificación de la base de datos MNIST")
   st.markdown("Sube una imagen para clasificar")
 
-  uploaded_file = st.file_uploader("Selecciona una imagen (PNG, JPG, JPEG:)", type = ["jpg", "png", "jpeg"])
+  uploaded_file = st.file_uploader("S
+import streamlit as st
+from PIL import Image
+from tensorflow.keras.preprocessing.image import img_to_array
+
+def preprocess_image(image):
+  image = image.convert('L') # convertir a escala de grises
+  image = image.resize((28,28))
+  image_array = img_to_array(image) / 255.0
+  image_array = np.expand_dims(image_array, axis=0)
+  return image_array
+
+def main():
+  st.title("Clasificación de la base de datos MNIST")
+  st.markdown("Sube una imagen para clasificar")
+
+  uploaded_file = st.file_uploader("S
+elecciona una imagen (PNG, JPG, JPEG:)", type = ["jpg", "png", "jpeg"])
 
   if uploaded_file is not None:
     image = Image.open(uploaded_file)
@@ -23,6 +40,7 @@ def main():
     preprocessed_image = preprocess_image(image)
     
     st.image(preprocessed_image, caption = "imagen subida")
+  
 
 if __name__ == "__main__":
   main()
