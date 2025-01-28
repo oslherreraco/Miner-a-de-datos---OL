02ab7be48826210e1d4b9a0486a0a75b987e019a
oslherreraco <oslherreraco@unal.edu.co>
2025-01-28 18:12:30 -0500

    Update aplicacion1.py

--- aplicacion1.py
+++ aplicacion1.py
@@ -1,8 +1,8 @@
 import streamlit as st
 
 def main():
-  st.title("Clasificación de la base de datos Mnist")
+  st.title("Clasificación de la base de datos MNIST")
   st.markdown("Sube una imagen para clasificar")
 
-if__name__=='main':
+if __name__ == "__main__":
   main()
