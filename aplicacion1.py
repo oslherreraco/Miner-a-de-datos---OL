f1653697525095bca8a2d960dac5523ebdde58ec
oslherreraco <oslherreraco@unal.edu.co>
2025-01-28 18:59:41 -0500

    Update aplicacion1.py

--- aplicacion1.py
+++ aplicacion1.py
@@ -1,6 +1,10 @@
 import streamlit as st
 from PIL import Image
 from tensorflow.keras.preprocessing.image import img_to_array
+import numpy as np
+import gzip
+import pickle
+import sklearn
 
 def preprocess_image(image):
   image = image.convert('L') # convertir a escala de grises
@@ -8,6 +12,12 @@ def preprocess_image(image):
   image_array = img_to_array(image) / 255.0
   image_array = np.expand_dims(image_array, axis=0)
   return image_array
+
+def load_model():
+  filename = "model_trained.pkl.gz"
+  with gzip.open(filename, 'rb') as f:
+    model = pickle.load(f)
+  return model
 
 def main():
   st.title("Clasificación de la base de datos MNIST")
@@ -19,13 +29,17 @@ def main():
     image = Image.open(uploaded_file)
     st.image(image, caption = "imagen subida")
 
-    preprocessed_image = preprocess_image(image)
-    
+    preprocessed_image = preprocess_image(image) # (1,28,28)
+   
     st.image(preprocessed_image, caption = "imagen subida")
 
     if st.button("Clasificar imagen"):
-st.markdown("Imagen clasificada")
-  
+      st.markdown("Imagen clasificada")
+      model = load_model()
+
+      prediction = model.predict(preprocessed_image.reshape(1,-1)) # (1, 784)
+      st.markdown(f"La imagen fue clasificada como: {prediction}")
+
 
 if __name__ == "__main__":
   main()
