2acc4b24b9c2761b82fe83e2f1632d31ae72177a
oslherreraco <oslherreraco@unal.edu.co>
2025-01-28 18:24:19 -0500

    Update aplicacion1.py

--- aplicacion1.py
+++ aplicacion1.py
@@ -4,7 +4,7 @@ def main():
   st.title("Clasificación de la base de datos MNIST")
   st.markdown("Sube una imagen para clasificar")
 
-  uploaded_file=st.file.uploader("Selecciona una imagen PNG, JPG JEPG: "), type = ["jpg", "png", "jpeg"])
+  uploaded_file = st.file_uploader("Selecciona una imagen (PNG, JPG, JPEG:)", type = ["jpg", "png", "jpeg"])
 
 if __name__ == "__main__":
   main()
