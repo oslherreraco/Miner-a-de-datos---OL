46fba0cd757348009c21c1c68fd8aa76bc89467e
oslherreraco <oslherreraco@unal.edu.co>
2025-01-30 15:56:25 -0500

    Update aplicacion.py

--- aplicacion.py
+++ aplicacion.py
@@ -21,7 +21,7 @@ def load_model():
 
 def main():
   st.title("Clasificación de la base de datos MNIST")
-  st.markdown("Sube una imagen para clasificar a")
+  st.markdown("Sube una imagen para clasificar")
 
   uploaded_file = st.file_uploader("Selecciona una imagen (PNG, JPG, JPEG:)", type = ["jpg", "png", "jpeg"])
 
