700e41ff75e8a36c2f065fdec5068aadf7e20ea5
oslherreraco <oslherreraco@unal.edu.co>
2025-01-31 15:10:16 -0500

    Update Taller22Mineria.py

--- Taller22Mineria.py
+++ Taller22Mineria.py
@@ -30,24 +30,32 @@ def predict_price(model):
     # Explicación breve
     st.write("Introduce los datos de la vivienda para estimar su precio promedio.")
     
-    # Crear el formulario de entrada en 3 columnas
+    # Crear el formulario de entrada con 3 columnas
     cols = st.columns(3)
     
     # Inicializar el diccionario para capturar los valores de entrada
     if 'inputs' not in st.session_state:
         st.session_state.inputs = {col: 0.0 for col in columns}
 
+    # Asignamos los inputs a las columnas en el orden de izquierda a derecha
     with cols[0]:
-        for i, col in enumerate(columns[0:5]):  # Primera columna con 5 variables
-            st.session_state.inputs[col] = st.number_input(f"{col} (Variable)", value=st.session_state.inputs[col], step=0.1)
-    
+        st.session_state.inputs["CRIM"] = st.number_input("CRIM", value=st.session_state.inputs["CRIM"], step=0.1)
+        st.session_state.inputs["ZN"] = st.number_input("ZN", value=st.session_state.inputs["ZN"], step=0.1)
+        st.session_state.inputs["INDUS"] = st.number_input("INDUS", value=st.session_state.inputs["INDUS"], step=0.1)
+        st.session_state.inputs["CHAS"] = st.number_input("CHAS", value=st.session_state.inputs["CHAS"], step=0.1)
+        st.session_state.inputs["NOX"] = st.number_input("NOX", value=st.session_state.inputs["NOX"], step=0.1)
+
     with cols[1]:
-        for i, col in enumerate(columns[5:10]):  # Segunda columna con 5 variables
-            st.session_state.inputs[col] = st.number_input(f"{col} (Variable)", value=st.session_state.inputs[col], step=0.1)
+        st.session_state.inputs["RM"] = st.number_input("RM", value=st.session_state.inputs["RM"], step=0.1)
+        st.session_state.inputs["AGE"] = st.number_input("AGE", value=st.session_state.inputs["AGE"], step=0.1)
+        st.session_state.inputs["DIS"] = st.number_input("DIS", value=st.session_state.inputs["DIS"], step=0.1)
+        st.session_state.inputs["RAD"] = st.number_input("RAD", value=st.session_state.inputs["RAD"], step=0.1)
+        st.session_state.inputs["TAX"] = st.number_input("TAX", value=st.session_state.inputs["TAX"], step=0.1)
 
     with cols[2]:
-        for i, col in enumerate(columns[10:]):  # Tercera columna con 3 variables
-            st.session_state.inputs[col] = st.number_input(f"{col} (Variable)", value=st.session_state.inputs[col], step=0.1)
+        st.session_state.inputs["PTRATIO"] = st.number_input("PTRATIO", value=st.session_state.inputs["PTRATIO"], step=0.1)
+        st.session_state.inputs["B"] = st.number_input("B", value=st.session_state.inputs["B"], step=0.1)
+        st.session_state.inputs["LSTAT"] = st.number_input("LSTAT", value=st.session_state.inputs["LSTAT"], step=0.1)
     
     # Botón para limpiar los datos
     if st.button("Limpiar los datos"):
