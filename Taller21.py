954e909d3e82eccc504ab1a45c4ae4cfb5e9f12d
oslherreraco <oslherreraco@unal.edu.co>
2025-01-30 21:17:30 -0500

    Update Taller21.py

--- Taller21.py
+++ Taller21.py
@@ -4,7 +4,6 @@ from tensorflow.keras.preprocessing.image import img_to_array
 import numpy as np
 import gzip
 import pickle
-import sklearn
 import pandas as pd  # Importamos pandas para manejar las tablas
 
 # Función para preprocesar la imagen
@@ -66,8 +65,14 @@ def main():
                     # Convertir los hiperparámetros a un formato adecuado para una tabla
                     model_params_table = [(key, value) for key, value in model_params.items()]
 
+                    # Reemplazar <NA> o None por un guion o valor vacío
+                    cleaned_model_params = [
+                        (key, value if value is not None and value != "<NA>" else "-") 
+                        for key, value in model_params_table
+                    ]
+
                     # Convertir a un DataFrame de pandas para tener control sobre la tabla
-                    df = pd.DataFrame(model_params_table, columns=["Hiperparámetro", "Valor"])
+                    df = pd.DataFrame(cleaned_model_params, columns=["Hiperparámetro", "Valor"])
 
                     # Mostrar la tabla con los hiperparámetros
                     st.table(df)  # Mostrar la tabla con nombre de columnas
